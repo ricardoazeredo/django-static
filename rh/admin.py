d8daa4e6513c851ec0644f9e172b8742c69ae43f
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2025-11-05 16:20:43 -0300

    update: atualizando a área administrativa de funcionarios

--- rh/admin.py
+++ rh/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Funcionarios
 
-# Register your models here.
-admin.site.register(Funcionarios)
+@admin.register(Funcionarios)
+class FuncionariosAdmin(admin.ModelAdmin):
+    # Quais colunas mostrar na lista de produtos
+    list_display = ('nome', 'cargo', 'departamento', 'data_contratacao','status')
+    # Por quais campos podemos buscar
+    search_fields = ("nome",)
+    # Quais campos podemos filtrar
+    list_filter = ('status', 'data_contratacao')
+    
+# admin.site.register(Funcionarios)
 
 
     
