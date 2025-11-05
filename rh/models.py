fa4dfa2c432e1d894f0c5ca35010c8577ee92a78
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2025-11-05 15:46:12 -0300

    update: corrigindo o problema do plural da classe no admin

--- rh/models.py
+++ rh/models.py
@@ -8,5 +8,10 @@ class Funcionarios(models.Model):
     departamento = models.CharField(max_length=100)
     data_contratacao = models.DateField()
     status = models.BooleanField(default=True)
+    
+    #corrige o problema de duplo 's' no nome do modelo no admin
+    class Meta:
+        verbose_name = "Funcionário"
+        verbose_name_plural = "Funcionários" # Define o nome plural correto
     def __str__(self):
         return self.nome
